3c5c111cbf922918b4669c4f8b0a6d7ee60f3254
Gianni Scolaro <40976911+giannidsp@users.noreply.github.com>
2018-08-04 23:37:52 -0400

    2 botones multiplayer

---
+++ Tema1/Assets/Ball.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour {
+
+	int speedX;
+	int speedY;
+
+	float speed;
+
+	//PUNTUACION
+
+	public Text scoreText;
+
+	int player1Score;
+	int player2Score;
+
+	//GANADOR
+
+	public Text winner;
+
+
+	public ParticleSystem sparks;
+
+	// Use this for initialization
+	void Start () {
+
+		MoveBall ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		scoreText.text = player1Score.ToString () + " - " + player2Score.ToString ();
+
+		if (player1Score == 3) {
+			winner.text = "PLAYER 1 GANA";
+			winner.gameObject.SetActive (true);
+			ResetBall ();
+		}
+
+		if (player2Score == 3) {
+			winner.text = "PLAYER 2 GANA";
+			winner.gameObject.SetActive (true);
+			ResetBall ();
+		}
+	}
+
+	void ResetBall(){
+		transform.localPosition = new Vector3 (0, 0, 0);
+		GetComponent<Rigidbody> ().velocity = Vector3.zero;
+	}
+
+	void MoveBall(){
+	
+		speed = Random.Range (5, 10);
+
+		speedX = Random.Range (0, 2);
+		if (speedX == 0) {
+			speedX = 1;
+		} else {
+			speedX = -1;
+		}
+
+		speedY = Random.Range (0, 2);
+		if (speedY == 0) {
+			speedY = 1;
+		} else {
+			speedY = -1;
+		}
+
+
+		GetComponent<Rigidbody> ().velocity = new Vector3 (speed * speedX, speed * speedY, 0);
+	}
+
+	void OnCollisionEnter(Collision objeto){
+
+		if (objeto.collider.tag == "player1goal") {
+			player1Score++;
+			ResetBall ();
+			Invoke("MoveBall",2);
+		}
+
+		if (objeto.collider.tag == "player2goal") {
+			player2Score++;
+			ResetBall ();
+			Invoke("MoveBall",2);
+		}
+
+		if (objeto.collider.tag == "Player") {
+			sparks.Play ();
+		}
+
+	}
+}
---
+++ Tema1/Assets/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public KeyCode up;
+	public KeyCode down;
+
+	public KeyCode space;
+
+	float speed;
+	float finalSpeed;
+
+	// Use this for initialization
+	void Start () {
+		speed = 0.45f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKey (up)) {
+
+			if (transform.localPosition.y > 4) {
+				finalSpeed = 0;
+			} else {
+				finalSpeed = speed;
+			}
+
+			transform.Translate (0, finalSpeed, 0);
+		}
+
+
+		if (Input.GetKey (down)) {
+
+			if (transform.localPosition.y < -4) {
+				finalSpeed = 0;
+			} else {
+				finalSpeed = speed;
+			}
+				
+			transform.Translate (0, -finalSpeed, 0);
+		}
+
+		if (Input.GetKey (space)) {
+
+			Application.LoadLevel (Application.loadedLevel);
+		
+		}
+
+	}
+}
